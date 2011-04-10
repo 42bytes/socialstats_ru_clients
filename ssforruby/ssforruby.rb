61ad94d3bf9b26ece84fe6448ca3fded48fcee97
Sergio Tulentsev <sergei.tulentsev@gmail.com>
2011-04-10 22:28:20 +0400

    Add client for Ruby.

---
+++ ssforruby/ssforruby.rb
@@ -0,0 +1,42 @@
+require 'uri'
+require 'json'
+
+class SSTracker
+  def initialize swf_id, swf_key
+    @swf_id = swf_id
+    @swf_key = swf_key
+  end
+
+  def track_event name, value = nil
+    parms = {:act => name}
+
+    if value
+      parms[:val] = value
+      parms[:agg] = 'count'
+    end
+
+    send_secure_request 'track_event', parms
+  end
+
+
+  private
+  def send_secure_request method_name, additional_params = {}
+    parms = {:swf_id => @swf_id,
+             :vid => 'server',
+             :rid => rand}.merge(additional_params)
+    parms[:sig] = get_signature(parms)
+    url = "http://socialstats.ru/flash/#{method_name}"
+    return JSON.parse(Net::HTTP.post_form(URI.parse(url), parms.stringify_keys).body)
+  end
+
+
+  def get_signature parameters = {}
+    str = "server"
+    parameters.stringify_keys.sort.each do |k, v|
+      str << "#{k}=#{v}"
+    end
+    str << @swf_key
+
+    Digest::MD5.hexdigest(str)
+  end
+end
